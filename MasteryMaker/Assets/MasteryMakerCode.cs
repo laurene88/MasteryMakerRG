9b04673680c0175367d5185f8bb9b4e2db222ebf
laurene88 <lauren.eddington@hotmail.com>
2023-05-22 15:06:30 +1200

    Results page added

--- MasteryMaker/Assets/MasteryMakerCode.cs
+++ MasteryMaker/Assets/MasteryMakerCode.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MasteryMakerCode : MonoBehaviour
 {
     [SerializeField]
     public GameObject ApparatusButtonPanel;
     private bool ApparatusButtonPanelVisible = false;
+
+
     
     void Start ()
     {
-
+     
     }
     
-    
+    //MENUPAGE SCENE
     public void PressApparatusButton ()
     {
         ApparatusButtonPanelVisible = !ApparatusButtonPanelVisible;
         ApparatusButtonPanel.SetActive(ApparatusButtonPanelVisible);
+    }
+
+    //RESULTSPAGE SCENE
+    public void PressReturnButton()
+    {
+        //loads in single mode automatically
+        SceneManager.LoadScene(0);
     }
 
 }
